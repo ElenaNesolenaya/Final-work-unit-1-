e5c782aa2b16971587e4ddbe50f4f570e996b5ac
ElenaNesolenaya <122355976+ElenaNesolenaya@users.noreply.github.com>
2023-01-25 20:17:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массива
+
+string[] array = new string[] { "Kazan", "SPB", "Russia", "1234", "567" };
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void Diffarray(string[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] =  string.Empty;
+    }
+}
+PrintArray(array);
+Diffarray(array);
+Console.Write("-> ");
+PrintArray(array);
